ff08e8e24dfa646fc033815941631af7412a81c6
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 19:33:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -109,5 +109,16 @@ def checkout(cart, coupons)
   # some irritated customers
 end
 
-kart = 
+kart = [
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "KALE", :price => 3.00, :clearance => false},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "ALMONDS", :price => 9.00, :clearance => false},
+      {:item => "TEMPEH", :price => 3.00, :clearance => true},
+      {:item => "CHEESE", :price => 6.50, :clearance => false},
+      {:item => "BEER", :price => 13.00, :clearance => false},
+      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+      {:item => "BEETS", :price => 2.50, :clearance => false},
+      {:item => "SOY MILK", :price => 4.50, :clearance => true}
+    ]
 koupons = 
