d91936b429bb8e9e251ab2394402f2621e359653
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 19:41:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -97,17 +97,6 @@ def checkout(cart, coupons)
   else
     young_money_cash_money
   end
-  
-  
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
 end
 
 kart = [
