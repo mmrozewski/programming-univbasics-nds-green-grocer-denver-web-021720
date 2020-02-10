f81b0d51a20d7cc52119239dedfbcb7babe9b67e
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 19:38:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -81,7 +81,7 @@ end
 def checkout(cart, coupons)
   
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   mint_fresh_cart = apply_clearance(couponed_cart)
   
   puts mint_fresh_cart
