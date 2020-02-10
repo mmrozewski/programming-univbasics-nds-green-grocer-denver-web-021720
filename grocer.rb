0aa600f9aa333f71233efe56e118e9c6989606c3
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 19:53:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,7 +71,7 @@ def apply_clearance(cart)
   index = 0
   while index < cart.length do
     if cart[index][:clearance]
-      cart[index][:price] /= 1.2.round(2)
+      cart[index][:price] /= 1.20.round(2)
     end
     index += 1
   end 
